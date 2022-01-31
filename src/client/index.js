3aa9a11b5693bf6e23ce2b6f54cd01bc709f54ce
Runchi <runchidasemail@gmail.com>
2022-01-31 13:35:05 +0100

    - initial commit, set project structure

---
+++ src/client/index.js
@@ -0,0 +1,7 @@
+// import styles
+import './styles/base.scss'
+
+// import js
+import './js/app'
+
+console.log('indox.js loaded')
---
+++ src/server/server.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const dotenv = require('dotenv')
+const axios = require('axios')
+dotenv.config();
+
+// setup express server
+const port = '3000'
+const app = express()
+app.use(cors())
+app.use(express.static(__dirname + '/../../dist/'))
+app.use(bodyParser.json())
+
+app.listen(port, () => {
+    console.log('Server running on port: ' + port)
+})
+
+app.get('/', (req, res) => {
+    res.sendFile('/dist/index.html', { root: __dirname + '/../..' })
+})
