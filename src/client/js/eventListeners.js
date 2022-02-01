ecdb92ee29afe9537cb2c498f1207c75e8270eae
Runchi <runchidasemail@gmail.com>
2022-02-01 14:23:39 +0100

    - add countdown to trip day

--- src/client/js/eventListeners.js
+++ src/client/js/eventListeners.js
@@ -1,17 +1,32 @@
+let todayDate = getDate();
+console.log(todayDate)
+
 window.addEventListener('load', () => {
     document.getElementById('travelForm').addEventListener('submit', onSubmit)
     document.getElementById('travelButton').addEventListener('click', onSubmit)
+    document.getElementById('date').setAttribute('min', todayDate)
     console.log('Event listeners added')
+
 })
 
 export function onSubmit(event) {
     event.preventDefault();
     if (document.getElementById('travelForm').checkValidity()) {
-        const tripInfo = { location: document.getElementById('location').value, date: document.getElementById('date').value }
+        const tripInfo = { location: document.getElementById('location').value, date: document.getElementById('date').value, todayDate: document.getElementById('date').min }
         console.log(tripInfo)
         Client.postInfo('/takeoff', tripInfo)
     }
     else {
         document.getElementById('result').innerHTML = 'Tell me when, tell me where!'
     }
+}
+
+function getDate() {
+    var today = new Date();
+    let month = today.getMonth() + 1;
+    let date = 0;
+    if (today.getMonth() + 1 < 10) { month = '0' + month }
+    if (today.getDate() < 10) { date = '0' + today.getDate() }
+    var todayDate = today.getFullYear() + '-' + month + '-' + date;
+    return todayDate;
 }
--- src/server/server.js
+++ src/server/server.js
@@ -3,6 +3,7 @@ const cors = require('cors')
 const bodyParser = require('body-parser')
 const dotenv = require('dotenv')
 const axios = require('axios')
+const xml2js = require('xml2json')
 dotenv.config();
 
 // setup express server
@@ -11,6 +12,9 @@ const app = express()
 app.use(cors())
 app.use(express.static(__dirname + '/../../dist/'))
 app.use(bodyParser.json())
+
+// server data
+let timeToTrip;
 
 app.listen(port, () => {
     console.log('Server running on port: ' + port)
@@ -25,16 +29,21 @@ app.post('/takeoff', onPost)
 async function onPost(req, res) {
     res.send({ request: req.body })
     const tripInfo = req.body
-    const resGeo = await getLocation(tripInfo)
-    try {
-        console.log(resGeo.data)
-    }
-    catch (error) {
-        console.log('Problem connecting to API')
-        console.log(error)
-    }
-}
+    timeToTrip = getTimeToTrip(req.body.date)
+    console.log(timeToTrip)
 
+    // const resGeo = await getLocation(tripInfo)
+    // try {
+    //     // console.log(resGeo.data)
+    //     const geoJson = JSON.parse(xml2js.toJson(resGeo.data))
+    //     const coord = { lat: geoJson.geonames.code[0].lat, lng: geoJson.geonames.code[0].lng }
+    //     console.log(coord)
+    // }
+    // catch (error) {
+    //     console.log('Problem connecting to API')
+    //     console.log(error)
+    // }
+}
 
 // API caller functions
 async function getLocation(tripInfo) {
@@ -54,4 +63,17 @@ async function getWeather(tripInfo) {
 async function getPicture(tripInfo) {
     const baseUrl = "https://pixabay.com/api/"
     // get
+}
+
+// helper function for time
+function getTimeToTrip(tripDate) {
+    const todayDate = (() => {
+        const todayTime = new Date()
+        return new Date(todayTime.getFullYear(), todayTime.getMonth(), todayTime.getDate())
+    })()
+    const splitDate = tripDate.split('-')
+    const planDate = new Date(splitDate[0], splitDate[1] - 1, splitDate[2])
+    const diff = planDate - todayDate
+    console.log('frontend:' + tripDate)
+    return Math.floor((diff / 3600 / 24 / 1000))
 }
